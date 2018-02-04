c940e210eda48bbfbac69c0c5ddc5d82706934be
Viacheslav Kostechko <viachelslav@timyo.com>
2018-02-04 19:47:35 +0300

    project setup

---
+++ TMProgressView/TMProgressView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TMProgressView
+//
+//  Created by Viachaslau Kastsechka on 2/4/18.
+//  Copyright © 2018 Viachaslau Kastsechka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var progressView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+   
+    @IBAction func startAction(_ sender: Any) {
+    }
+    
+    @IBAction func stopAction(_ sender: Any) {
+    }
+    
+}
+
